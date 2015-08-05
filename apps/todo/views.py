c1d2f506ef38118d27bd86f3cd5962d95f8879c9
onlyanyz <onlyanyz@foxmail.com>
2015-08-06 00:23:45 +0800

    更新todo：添加注册，只有一点

--- apps/todo/views.py
+++ apps/todo/views.py
@@ -101,4 +101,6 @@ def login_page(request):
     return render_to_response('login.html',context_instance=RequestContext(request))
 
 def register(request):
+    if request.method=='POST':
+        pass
     return render_to_response('register.html',context_instance=RequestContext(request))
